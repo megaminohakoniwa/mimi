e7caa69e1a0522a06f5de8d8b0902fb9360d067a
YUKI <yuki.acc@icloud.com>
2026-08-27 14:01:16 +0900

    Remove obsolete UX stylesheet import

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import Home from '../app/page';
 import '../app/globals.css';
-import '../app/ux-fixes.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
